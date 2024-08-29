VaneBrain/scpntf

commit 878e575b340b070f15bf21842b787f6ed277afbf

- The checkpoint elevators now reset their position if player used the elevator but was captured by SCP-106.

SourceCode/MTF.bb
@@ -1639,6 +1639,26 @@ Function UpdateNewElevators()
 	
 End Function
 
+Function ResetNewElevator(ne.NewElevator, default_floor% = 1)
+	If ne <> Null Then
+		If PlayerNewElevator = ne\ID Then
+			PlayerNewElevator = 0
+			PlayerInNewElevator = False
+		EndIf
+		ne\state = 0.0
+		ne\currfloor = default_floor
+		ne\tofloor = default_floor
+		ne\door\open = True
+		PositionEntity ne\obj,EntityX(ne\obj),ne\floory[default_floor-1],EntityZ(ne\obj)
+		PositionEntity ne\door\frameobj,EntityX(ne\door\frameobj),EntityY(ne\obj,True),EntityZ(ne\door\frameobj)
+		PositionEntity ne\door\obj,EntityX(ne\door\obj),EntityY(ne\obj,True),EntityZ(ne\door\obj)
+		PositionEntity ne\door\obj2,EntityX(ne\door\obj2),EntityY(ne\obj,True),EntityZ(ne\door\obj2)
+		PositionEntity ne\door\buttons[0],EntityX(ne\door\buttons[0]),EntityY(ne\obj,True)+0.6,EntityZ(ne\door\buttons[0])
+		PositionEntity ne\door\buttons[1],EntityX(ne\door\buttons[1]),EntityY(ne\obj,True)+0.7,EntityZ(ne\door\buttons[1])
+		StopStream_Strict(ne\soundchn)
+	EndIf
+End Function
+
 Function DeleteNewElevators()
 	Local ne.NewElevator
 	

SourceCode/Rooms/Checkpoints.bb
@@ -257,6 +257,22 @@ Function UpdateEvent_Checkpoints(e.Events)
 		EndIf
 	EndIf
 	
+	If PlayerRoom <> e\room And PlayerNewElevator <> 0 Then
+		For ne = Each NewElevator
+			If PlayerNewElevator = ne\ID And ne\room = e\room Then
+				Select NTF_CurrZone
+					Case EZ
+						ResetNewElevator(ne, 3)
+					Case LCZ
+						ResetNewElevator(ne, 2)
+					Case HCZ
+						ResetNewElevator(ne, 1)
+				End Select
+				Exit
+			EndIf
+		Next
+	EndIf
+	
 End Function
 
 ;~IDEal Editor Parameters:

commit 0dadf8bf7480ef671981860a6f9c9d523139971f

- Fixed a bug that was introduced in commit 878e575. The game now resets the elevators if the player is outside the facility (like in Pocket Dimension) instead of the player being just outside the room. - Removed unused variable in StartNewElevator function. - Small optimization in Room1_Sewers elevator.

SourceCode/MTF.bb
@@ -1438,12 +1438,11 @@ Function CreateNewElevator.NewElevator(obj%,currfloor%,door.Doors,id#,r.Rooms,fl
 End Function
 
 Function StartNewElevator(door.Doors,newfloor%)
-	Local ne.NewElevator, ne_found.NewElevator
+	Local ne.NewElevator
 	Local playerinside% = False
 	
 	For ne = Each NewElevator
 		If ne\door = door
-			ne_found = ne
 			Exit
 		EndIf
 	Next
@@ -1626,6 +1625,7 @@ Function UpdateNewElevators()
 				ne\state = ne\state + FPSfactor
 			ElseIf ne\state >= 100.0 Then
 				UseDoor(ne\door)
+				PlayerNewElevator = 0
 				PlayerInNewElevator = False
 				ne\state = 0.0
 			EndIf

SourceCode/Rooms/Checkpoints.bb
@@ -263,7 +263,7 @@ Function UpdateEvent_Checkpoints(e.Events)
 		EndIf
 	EndIf
 	
-	If PlayerRoom <> e\room And PlayerNewElevator <> 0 Then
+	If (Not PlayerInReachableRoom()) Then
 		For ne = Each NewElevator
 			If PlayerNewElevator = ne\ID And ne\room = e\room Then
 				Select NTF_CurrZone

SourceCode/Rooms/Room1_Sewers.bb
@@ -177,18 +177,15 @@ Function UpdateEvent_Room1_Sewers(e.Events)
 			elevFloor = 2
 		EndIf
 		If (Not PlayerInNewElevator) Then
-			PositionEntity e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\frameobj,EntityX(e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\frameobj),elevY*RoomScale,EntityZ(e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\frameobj)
-			PositionEntity e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\obj,EntityX(e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\obj),elevY*RoomScale,EntityZ(e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\obj)
-			PositionEntity e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\obj2,EntityX(e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\obj2),elevY*RoomScale,EntityZ(e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\obj2)
-			PositionEntity e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\buttons[0],EntityX(e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\buttons[0]),elevY*RoomScale+0.6,EntityZ(e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\buttons[0])
-			PositionEntity e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\buttons[1],EntityX(e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\buttons[1]),elevY*RoomScale+0.7,EntityZ(e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\buttons[1])
 			For ne = Each NewElevator
 				If ne\door = e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID] Then
+					ResetNewElevator(ne, elevFloor)
 					If ne\currfloor = elevFloor And ne\state = 0.0 Then
 						e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\open = True
 					Else
 						e\room\RoomDoors[ROOM1_SEWERS_ELEVATOR_DOOR_ID]\open = False
 					EndIf
+					Exit
 				EndIf
 			Next
 		EndIf
@@ -315,6 +312,15 @@ Function UpdateEvent_Room1_Sewers(e.Events)
 		EndIf
 	Else
 		HideEntity e\room\Objects[ROOM1_SEWERS_ROOM_ID]
+	EndIf
+	
+	If (Not PlayerInReachableRoom()) Then
+		For ne = Each NewElevator
+			If PlayerNewElevator = ne\ID And ne\room = e\room Then
+				ResetNewElevator(ne, 2)
+				Exit
+			EndIf
+		Next
 	EndIf
 	
 End Function